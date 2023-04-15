7a2fa5dbbcc3243ed2abbb015eafd2606d1fd158
MeowSalsa <100064587+MeowSalsa@users.noreply.github.com>
2023-04-14 17:41:44 -0700

    VEN-93 refactored Cursor_tools.js to be able to be instantiated by Venus' main script. Also added Venus.js that could be used to add Venus' code. Modified Cursor_tools.js to contain a sleep timer so that we can't get points that are too close in time.

--- Game/src/Cursor_tools.js
+++ Game/src/Cursor_tools.js
@@ -3,17 +3,23 @@ class Cursor_tools {
 
   // Finding Velocity & Accleration JS Only 
   //Global Variables
-  static coordArray = [];
-  static accelerationsArray = [];
 
+  coordArray = [];
+  accelerationsArray = [];
+  abnormalAcceleration;
 
+  constructor() {
+    this.coordArray = [];
+    this.abnormalAcceleration = [];
+    this.abnormalAcceleration = false;
+  }
   //Mouse Movement Coordinates 
   /**
    * Captures the coordinates of the mouse when the event listener detects mouse movement on the game.
    * @param {Event} event the event passed by the eventListener on the detection of mouse movement. 
    * @returns A dictionary {x,y,time_ms} with a coordinate and time assiociated to it 
    */
-  static MouseMovement(event) {
+  MouseMovement(event) {
     let currentTime = Date.now();
     let currentMouseX = event.pageX;
     let currentMouseY = event.pageY;
@@ -28,14 +34,16 @@ class Cursor_tools {
   * @param {number} coord2 dictionary of {x,y,time_ms} where the time is the time this point was aquired.
   * @returns dictionary of {velocity, time} where the time is the delta time between the points.
   */
-  static calculateVelocity(coord1, coord2) {
+  calculateVelocity(coord1, coord2) {
     let movementX = Math.abs(coord1.x - coord2.x);
     let movementY = Math.abs(coord1.y - coord2.y);
     let distance = null;
     let velocity = null;
     let timeDifference = null;
     distance = Math.sqrt(movementX * movementX + movementY * movementY);
-    timeDifference = Math.abs(coord1.time_ms - coord2.time_ms);
+    console.log("coord2  " + coord2.time_ms);
+    console.log("Coord 1 " + coord1.time_ms);
+    timeDifference = Math.abs(coord2.time_ms - coord1.time_ms);
     velocity = Math.round(distance / timeDifference * 1000);
     //console.log(`Our start coordinates: (${startMouseX}, ${startMouseY})`)
     //console.log(`Our current coordinates: (${currentMouseX}, ${currentMouseY})`)
@@ -53,7 +61,7 @@ class Cursor_tools {
  * @param {number} finalVelocity dictionary of {velocity, time_delta} sent by historical acceleration calculator
  * @returns the acceleration between the two velocities.
  */
-  static calculateAcceleration(startVelocity, finalVelocity) {
+  calculateAcceleration(startVelocity, finalVelocity) {
     console.log("Calculating Acceleration method");
     let startTime = 0;
     let finalTime = finalVelocity.time_delta;
@@ -70,17 +78,18 @@ class Cursor_tools {
   * @returns true if the current acceleration is determined to be abnormal
   * otherwise, returns false.
   */
-  static historicalAccelerationComparison() {
+  historicalAccelerationComparison() {
     const maxAccelerationData = 100;
-    var abnormalAcceleration = false;
-    var velocity1 = this.calculateVelocity(this.coordArray[0], this.coordArray[1]); //I BELIEVE THIS SHOULD RETURN [VELOCITY,DELTA_T]
-    var velocity2 = this.calculateVelocity(this.coordArray[2], this.coordArray[3]); //I BELIEVE THIS SHOULD RETURN [VELOCITY,DELTA_T]
+    var velocity1 = this.calculateVelocity(this.coordArray[0], this.coordArray[1]);
+    var velocity2 = this.calculateVelocity(this.coordArray[2], this.coordArray[3]);
     var currAcceleration = this.calculateAcceleration(velocity1, velocity2);
+    var maxAccelRatio = 1.5;
+    var minAccelRatio = 0.5;
     //var acceleration = 1000; //some testing value
     if (this.accelerationsArray.length != maxAccelerationData) {
       this.accelerationsArray.push(currAcceleration);
     } else {
-      console.log("Went into 101st acceleration data\n");
+      //console.log("Went into 101st acceleration data\n");
       var accelerationSum = 0;
       //calculate sum of accelerations in historical_accelerations list
       for (var i = 0; i < this.accelerationsArray.length; i++) {
@@ -91,8 +100,9 @@ class Cursor_tools {
       var accelerationAverage = accelerationSum / maxAccelerationData;
       //calculate ratio between current acceleration and the average
       var accelerationRatio = currAcceleration / accelerationAverage;
-      if (accelerationRatio >= 3) {
-        abnormalAcceleration = true;
+      if (accelerationRatio < minAccelRatio || accelerationRatio >= maxAccelRatio) {
+        //console.log("ABNORMAL ACCEL!!!!!!");
+        this.abnormalAcceleration = true;
       }
       //pop the first value in historical_accelerations, shift left, add new value to end of list.
       this.accelerationsArray.shift();
@@ -100,10 +110,10 @@ class Cursor_tools {
       //clear the current coordinates
     }
     this.coordArray = [];
-    return abnormalAcceleration;
+    //return abnormalAcceleration;
   }
 
-  static handleMouseData(mousedata) {
+  handleMouseData(mousedata) {
     const maxCoords = 4;
     if (this.coordArray.length == maxCoords - 1) {
       console.log("Coordinate Array had 3 items, adding one more and moving to historical acceleration comparison");
@@ -116,8 +126,3 @@ class Cursor_tools {
     }
   }
 }
-
-//*....................................................................*
-document.addEventListener('mousemove', function (event) {
-  Cursor_tools.handleMouseData(Cursor_tools.MouseMovement(event));
-});
