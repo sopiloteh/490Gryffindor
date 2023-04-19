eb778363bff52126585dd53a6689ec1c2d063628
MeowSalsa <100064587+MeowSalsa@users.noreply.github.com>
2023-04-19 16:38:20 -0700

    VEN-106 added getPlayerPos function that gets the players x,y tile coordinates and the room they are currently in

--- Game/src/Venus.js
+++ Game/src/Venus.js
@@ -1,3 +1,9 @@
+//Function that gets the players' X,Y positions and the room they are current in.
+function getPlayerPos() {
+  var x = $('#player').attr('data-x');
+  var y = $('#player').attr('data-y');
+  var room = $.jStorage.get('is_in');
+}
 
 const test = new Cursor_tools();
 document.addEventListener('mousemove', function (event) {
