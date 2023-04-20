3dcd677878c2c451c57117a34036a6b0856dec32
sopiloteh <33475852+sopiloteh@users.noreply.github.com>
2023-04-20 14:40:41 -0700

    updates
    updated drop down buttons to get functions along with a console log as a substitute place holder for the eventual backend code

--- Game/src/Venus.js
+++ Game/src/Venus.js
@@ -1,4 +1,3 @@
-
 const test = new Cursor_tools();
 document.addEventListener('mousemove', function (event) {
   test.handleMouseData(test.MouseMovement(event));
@@ -12,10 +11,16 @@ document.addEventListener('mousemove', function (event) {
 
 // Set initial state to "On"
 let isVenusOn = true;
+let isClearOn = false;
+let isReadOn = false;
+let isResetOn = false;
 
 // Get references to Venus box and toggle button
 var venusBox = document.getElementById('venus_box');
 var toggleBtn = document.getElementById('toggleBtn');
+
+// Set initial reading size to "small"
+let readingSize = 'small';
 
 function toggleVenus() {
   // Toggle state
@@ -30,5 +35,53 @@ function toggleVenus() {
     toggleBtn.textContent = 'Sleep';
     venusBox.style.display = 'block';
     console.log("sleeeppy Venus");
+  }
+}
+
+function toggleClear(){
+  isClearOn = !isClearOn;
+
+  if(isClearOn){
+    toggleBtn.textContent = 'Cleared';
+    venusBox.style.display = 'block';
+    console.log("Data is cleared");
+  }else{
+    toggleBtn.textContent = 'Not Cleared';
+    venusBox.style.display = 'block';
+    console.log("Data not cleared");    
+  }
+}
+
+function toggleReading(){
+  isReadOn = !isReadOn;
+
+  // Toggle reading size between "small", "medium", and "large"
+  if (isReadOn) {
+    switch (readingSize) {
+      case 'small':
+        readingSize = 'medium';
+        break;
+      case 'medium':
+        readingSize = 'large';
+        break;
+      case 'large':
+        readingSize = 'small';
+        break;
+    }
+    console.log(`Reading size: ${readingSize}`);
+  }
+}
+
+function toggleReset(){
+  isResetOn = !isResetOn;
+
+  if(isResetOn){
+    toggleBtn.textContent = 'Reset Screen';
+    venusBox.style.display = 'block';
+    console.log("Screen was reset");
+  }else{
+    toggleBtn.textContent = 'Not Reset';
+    venusBox.style.display = 'block';
+    console.log("Screen is default");    
   }
 }
